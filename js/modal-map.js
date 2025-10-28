04514fda7c75a01aae9d90c86ba8a6bdb46b5558
Oleksandr <blackaslife2005@gmail.com>
2025-10-28 18:46:14 +0000

    overlay for modal-map

--- js/modal-map.js
+++ js/modal-map.js
@@ -1,3 +1,4 @@
+const overlay = document.querySelector(".modal-overlay");
 const mapLink = document.querySelector(".contacts img");
 const mapPopup = document.querySelector(".modal-map");
 
@@ -6,12 +7,20 @@ if (mapLink && mapPopup) {
 
   mapLink.addEventListener("click", function (evt) {
     evt.preventDefault();
+    overlay.classList.remove("visually-hidden");
     mapPopup.classList.remove("visually-hidden");
     mapPopup.classList.add("modal-animation");
   });
 
   mapClose.addEventListener("click", function (evt) {
     evt.preventDefault();
+    overlay.classList.add("visually-hidden");
+    mapPopup.classList.add("visually-hidden");
+    mapPopup.classList.remove("modal-animation");
+  });
+
+  overlay.addEventListener("click", function () {
+    overlay.classList.add("visually-hidden");
     mapPopup.classList.add("visually-hidden");
     mapPopup.classList.remove("modal-animation");
   });
